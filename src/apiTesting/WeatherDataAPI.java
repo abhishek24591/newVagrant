1d384d2ed26820b039e441aff1aa632d4c256bfe
abhishek24591 <abhidsgsg@gmail.com>
2020-09-20 23:30:24 +0530

    20 Sept 2020 second commit

--- src/apiTesting/WeatherDataAPI.java
+++ src/apiTesting/WeatherDataAPI.java
@@ -13,13 +13,11 @@ public class WeatherDataAPI extends Utility{
 		// TODO Auto-generated method stub
 		RestAssured.baseURI = "https://api.openweathermap.org";
 		
-		Response response = given().log().all().queryParam("q", Utility.getProperty(config_file, "city_to_be_searched")).
+		Response response = given().log().all().queryParam("q","Ajmer").
 		queryParam("appid","7fe67bf08c80ded756e598d6f8fedaea")
 		.when().get("/data/2.5/weather").then().log().all().assertThat().statusCode(200).extract().response();
 		
-        JsonPath j = new JsonPath(response.toString());
-        System.out.println("Weather details of city --> " + Utility.getProperty(config_file, "city_to_be_searched") + " is below");
-        System.out.println(j.toString());
+        System.out.println(response);
 
 	}
 
